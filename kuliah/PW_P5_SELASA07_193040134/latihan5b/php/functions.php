93a235c901c861dcc750552418a95ea2ebcc229c
Zaskhasa_193040134 <Zaskhas@gmail.com>
2020-04-17 10:53:38 +0700

    Tugas Kuliah

---
+++ kuliah/PW_P5_SELASA07_193040134/latihan5b/php/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+//function untuk melakukan koneksi ke database
+function koneksi() {
+    $conn = mysqli_connect("127.0.0.1" , "root" , "") or die("Koneksi ke DB Gagal");
+    mysqli_select_db($conn, "tubes_193040134") or die("Database Salah!");
+    
+    return $conn;
+}
+
+//function untuk melakukan query ke database
+function query($sql){
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+?>
